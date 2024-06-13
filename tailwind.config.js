a7a77a95ddc3ee155e7036d8c8a6b956cf9e5693
akash-x1i <akashatcode@gmail.com>
2024-06-13 13:41:59 +0530

    hero bg added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: ["./index.html"],  /* path of the file to check for tw utility classes */
   theme: {
     screens: {
+      '8xl': '1360px',
       '3xs': '370px',
       '2xs': '410px',
       'xs': '524px',
